6ef1592ea78d85285e756a1768deeb8d53e92d4e
Roy Hairul <76144448+royhairul@users.noreply.github.com>
2022-03-21 10:44:38 +0700

    Add File from Local
    Copy file from local first time.

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,600;0,700;0,800;0,900;1,400;1,600;1,800;1,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./css/style.min.css">
+  <script src="https://kit.fontawesome.com/c80485df49.js" crossorigin="anonymous"></script>
+  <title>Semegah Library</title>
+</head>
+<body>
+  <!-- Header -->
+  <header>
+    <div class="logo">
+      <div class="logo-icon"></div>
+      <h2 class="logo-text"> Semegah Library</h2>
+    </div>
+    <nav class="nav">
+      <li class="nav-item">Beranda</li>
+      <li class="nav-item">Info</li>
+      <li class="nav-item">Event</li>
+      <li class="nav-item">Berita</li>
+      <li class="nav-item">Tentang</li>
+    </nav>
+    <a href="https://katalog.smkn1glagah.sch.id/" class="btn">Katalog Kami</a>
+  </header>
+
+  <!-- Main -->
+  <main>
+    <section class="karya-wrap">
+      <h1 class="karya-title">Karya Kami</h1>
+      <p class="karya-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas, delectus.</p>
+      <div class="karya-box-wrap">
+        <div class="karya-box-item">
+          <h2>Buku</h2>
+          <p>Cek Karya Buku Kami</p>
+          <i class="fa-solid fa-arrow-up-right-from-square"></i>
+        </div>
+        <div class="karya-box-item">
+          <h2>Karyaku</h2>
+          <p>Cek Karya Kami Yang Lain</p>
+          <i class="fa-solid fa-arrow-up-right-from-square"></i>
+        </div>
+      </div>
+    </section>
+    <section class="new-book-wrap"></section>
+    <section class="popular-book-wrap"></section>
+    <section class="event -wrap"></section>
+    <section class="author-wrap"></section>
+  </main>
+
+</body>
+</html>
